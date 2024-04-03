dfd55a80cdbab0c5bfc2920e63728a81db5db0d8
Muhammadwaqas786129 <156679291+Muhammadwaqas786129@users.noreply.github.com>
2024-04-03 10:23:07 -0700

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,52 +1,49 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 using namespace std;
-class marks 
-{
+class Student{
 	private:
-	int a,b, c;
+		string stdID, stdName, dept;
 	public:
-	void in()
-	{
-		cout<<"enter your number ";
-		cin>>a>>b>>c;
+		Student(){
+			stdID= "0";
+			stdName= "0";
+			dept="0";
+		}
+	    explicit Student(int stdid, string name){
+	    	stdID=stdid;
+	    	stdName=name;
+		}
+		Student(string Stdid, string Name, string DepT){
+	    	stdID=Stdid;
+	    	stdName=Name;
+			dept=DepT; 	
+		}
+		Student(string Name, string DepT){
+	    	stdName=Name;
+			dept=DepT; 	
+		}
 		
+		void display(){
+			cout<<"\nID: "<<stdID<<endl;
+			cout<<"Name: "<<stdName<<endl;
 		
-		
-		}	
-	int sum()
-	{
-		return a+b+c;
-		
-	}
-	float avg()
-	{
-	return (a+b+c)/3.0;
-	
-	
-	
-	   
-	}
+		}
+		void show(){
+			cout<<"\nName: "<<stdName<<endl;
+			cout<<"ID: "<<stdID<<endl;
+			cout<<"Department: "<<dept<<endl;
+		}
+		void print(){
+			
+			cout<<"\nName: "<<stdName<<endl;
+			cout<<"Department: "<<dept<<endl;
+		}
 };
-int main()
-{
-	marks m;
-	int a ;
-	float s;
-	m.in();
-	a=m.sum();
-	s=m.avg();
-	cout<<"the sum is "<<a<<endl;
-	cout<<"the avg is"<<s<<endl;
-	 
-	
-	
-	
-	
-	
-	
-	
-	
-	
+int main(){
+	Student obj1(12345, "Ali_Hassan"), obj2("23456", "Sheraz_Depp", "Computer_science"), obj3("Fahad_Abbas", "Computer_Science");
+	obj1.display();
+	obj2.show();
+	obj3.print();
+	return 0;
 }
-	
-
