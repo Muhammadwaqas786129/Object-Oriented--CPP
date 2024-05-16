710dce004e702e9f47eb85d59eb9adbafea8c39f
Muhammadwaqas786129 <156679291+Muhammadwaqas786129@users.noreply.github.com>
2024-05-16 10:47:28 -0700

    Add files via upload

--- waqas.cpp
+++ waqas.cpp
@@ -1,79 +1,29 @@
 #include<iostream>
 using namespace std;
-class simple
+class person
 {
-	protected:
-		int a,b;
+	private:
+		char name[50];
 		public:
-			simple()
-			{
-				a=b=0;
-				
-			}
 			void in()
 			{
-				cout<<"enter a=";
-				cin>>a;
-				cout<<"enter b=";
-				cin>>b;
+				cout<<"enter your name=";
+			cin>>name;
 			}
-			void add()
+			void show()
 			{
-				cout<<"a+b="<<a+b<<endl;
+				cout<<"your name is="<<name<<endl;
 			}
-			void sub()
-			{
-				cout<<"a-b="<<a-b<<endl;
-			}
-			void mul()
-			{
-				cout<<"a*b="<<a*b<<endl;
-			}
-			void div()
-			{
-				cout<<"a/b="<<a/b<<endl;
-			}
-		};
-		class complex:public simple
-		{
-			public:
-				void add()
-				{
-					if(a<=0||b<=0)
-					cout<<"invalid value"<<endl;
-					else
-					simple::add();
-				}
-				void sub()
-				{
-					if(a<=0||b<=0)
-					cout<<"invalid value"<<endl;
-					else
-					simple::sub();
-				}
-				void mul()
-				{
-				if(a<=0||b<=0)
-				cout<<"invalid value"<<endl;
-				else 
-				simple::mul(); 	
-				}
-				void div()
-				{
-					if(a<=0||b<=0)
-					cout<<"invalid value"<<endl;
-					else 
-					simple::div(); 
-				}
-				
 		};
 		int main()
 		{
-			complex obj;
-			obj.add();
-			obj.in();
-			obj.add();
-			obj.sub();
-			obj.mul();
-			obj.div();
-}
+			person *ptr[5];
+			for(int i=0;i<5;i++)
+			{
+				ptr[i]=new person;
+				ptr[i]->in();
+			}
+			for(int i=0;i<5;i++)
+			ptr[i]->show();
+		}
+	
