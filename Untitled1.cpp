0b3942119ff785810e49c7415ab44a306064449f
Muhammadwaqas786129 <156679291+Muhammadwaqas786129@users.noreply.github.com>
2024-05-16 09:37:54 -0700

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,49 +1,65 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 using namespace std;
-class Student{
-	private:
-		string stdID, stdName, dept;
-	public:
-		Student(){
-			stdID= "0";
-			stdName= "0";
-			dept="0";
-		}
-	    explicit Student(int stdid, string name){
-	    	stdID=stdid;
-	    	stdName=name;
-		}
-		Student(string Stdid, string Name, string DepT){
-	    	stdID=Stdid;
-	    	stdName=Name;
-			dept=DepT; 	
-		}
-		Student(string Name, string DepT){
-	    	stdName=Name;
-			dept=DepT; 	
-		}
+class person 
+{
+	protected:
+		int id;
+		char name[50],address[100];
+		public:
+			person()
+			{
+				id=0;
+				name[0]='\0';
+				address[0]='\0';
+				
+			}
+	void getinfo()
+	{
+		cout<<"enter your id ";
+		cin>>id;
+		cout<<"enter your name";
+		cin>>name;
+		cout<<"enter your address ";
+		cin>>address;
 		
-		void display(){
-			cout<<"\nID: "<<stdID<<endl;
-			cout<<"Name: "<<stdName<<endl;
-		
-		}
-		void show(){
-			cout<<"\nName: "<<stdName<<endl;
-			cout<<"ID: "<<stdID<<endl;
-			cout<<"Department: "<<dept<<endl;
-		}
-		void print(){
-			
-			cout<<"\nName: "<<stdName<<endl;
-			cout<<"Department: "<<dept<<endl;
-		}
+	}
+	void showinfo()
+	{
+		cout<<"your personal information are as follows "<<endl;
+		cout<<"your id="<<id<<endl;
+		cout<<"your name="<<name<<endl;
+		cout<<"your address="<<address<<endl;	
+	}
 };
-int main(){
-	Student obj1(12345, "Ali_Hassan"), obj2("23456", "Sheraz_Depp", "Computer_science"), obj3("Fahad_Abbas", "Computer_Science");
-	obj1.display();
-	obj2.show();
-	obj3.print();
-	return 0;
-}
+	class student:public person 
+	{
+		private:
+			int rollno,marks;
+			public:
+				student()
+				{
+					rollno=marks=0;
+				}
+				void getedu()
+				{
+					cout<<"enter your rollno=";
+					cin>>rollno;
+					cout<<"enter your marks=";
+					cin>>marks;
+					
+				}
+				void showedu()
+				{
+					cout<<"your educational information are as follows"<<endl;
+					cout<<"your rollno="<<rollno<<endl;
+					cout<<"your marks="<<marks;
+				}
+	};
+	int main()
+	{
+		student s;
+		s.getinfo();
+		s.getedu();
+		s.showinfo();
+		s.showedu();
+	}
